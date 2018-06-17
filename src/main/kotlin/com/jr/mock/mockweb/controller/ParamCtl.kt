81e3e8e08ef878227f86eeb91fc4b8c38a61401a
dzhh_cn <dzhh_cn@163.com>
2018-06-17 11:31:19 +0800

    parameters add children

--- src/main/kotlin/com/jr/mock/mockweb/controller/ParamCtl.kt
+++ src/main/kotlin/com/jr/mock/mockweb/controller/ParamCtl.kt
@@ -4,7 +4,6 @@ import com.jr.mock.mockweb.entity.Param
 import com.jr.mock.mockweb.repository.ParamRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Example
-import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -19,7 +18,7 @@ class ParamCtl {
     lateinit var paramRepository: ParamRepository
 
     @RequestMapping("list")
-    fun list(model: Model, param: Param):String {
+    fun list(model: Model, param: Param): String {
         param.model = "REQUEST"
         model.addAttribute("pageRequest", paramRepository.findAll(Example.of(param)))
         param.model = "RESPONSE"
@@ -28,17 +27,27 @@ class ParamCtl {
     }
 
     @GetMapping("save")
-    fun toSave(id: Int?, model: Model, param: Param):String {
+    fun toSave(id: Int?, model: Model, param: Param): String {
         model.addAttribute("parameter", param)
         if (id != null) {
             model.addAttribute("parameter", paramRepository.findById(id).get())
         }
-        model.addAttribute("abc", "阿根廷")
+        return "param/save"
+    }
+
+    @GetMapping("append")
+    fun toAppend(fid: Int, model: Model): String {
+        var parent = paramRepository.findById(fid).get()
+        var param = Param()
+        param.fid = fid
+        param.facadeId = parent.facadeId
+        param.model = parent.model
+        model.addAttribute("parameter", param)
         return "param/save"
     }
 
     @PostMapping("save")
-    fun doSave(param: Param):String {
+    fun doSave(param: Param): String {
         param.lastModifiedDate = Date()
         paramRepository.save(param)
         return "redirect:/facade/params?facadeId=${param.facadeId}"
--- src/main/kotlin/com/jr/mock/mockweb/entity/Param.kt
+++ src/main/kotlin/com/jr/mock/mockweb/entity/Param.kt
@@ -9,23 +9,21 @@ class Param: OperatorCommon() {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id:Int? = null
     @Column
-    var facadeId:Int? = null
+    var facadeId:Int? = null // 接口id
     @Column
-    var fid:String? = null
+    var fid:Int? = null // 父id
     @Column
     var name:String? = null
     @Column
     var desc:String? = null
     @Column
-    var model:String? = null
+    var model:String? = null // 参数、返回值
     @Column
-    var type:String? = null
+    var type:String? = null // 字符串、对象、数字、数字
     @Column
     var mock:String? = null
 
     override fun toString(): String {
         return "Param(id=$id, facadeId=$facadeId, fid=$fid, name=$name, desc=$desc, model=$model, type=$type, mock=$mock)"
     }
-
-
 }
